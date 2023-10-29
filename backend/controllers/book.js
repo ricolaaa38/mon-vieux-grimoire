987b267202bbebe0e46482088ba462d6045b899e
nicolas cretton <nicolas.cretton@orange.fr>
2023-10-29 15:52:49 +0100

    ajout erreur 403 auth sur route put

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -32,7 +32,7 @@ exports.modifyBook = (req, res, next) => {
     Book.findOne({_id: req.params.id})
     .then((book) => {
         if (book.userId != req.auth.userId) {
-            res.status(401).json({ message: 'Not authorized' });
+            res.status(403).json({ message: 'Unauthorized request' });
         } else {
             Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
             .then(() => res.status(200).json({ message: 'Objet modifié !' }))
